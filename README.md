526bca9490769ccbe1adc486bc8c3f2aff5406a2
sShaAanGg <sshaaangg.tw@gmail.com>
2023-01-31 16:47:19 +0800

    add test.c to test Clang along with LLVM
    Modify readme.md with environment information.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Clang-playground
+My Clang playground
+## Environment
+```shell
+❯ uname -r
+5.15.79.1-microsoft-standard-WSL2
+```
+```shell
+❯ clang --version
+clang version 16.0.0 (https://github.com/llvm/llvm-project.git e435f56815435422a63f5e9e9aad6959428e96c2)
+Target: x86_64-unknown-linux-gnu
+Thread model: posix
+InstalledDir: /home/shang/llvm/llvm-project/build/bin
+```
---
+++ test.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+char *global_str = "This is a string";
+int foo(int);
+
+int main()
+{
+    char **heap_str = (char **) malloc(sizeof(char) * 100);
+    *heap_str = "This is a string in the heap";
+    printf("The content of the string in the heap:   %s\n", *heap_str);
+    printf("The address of the string in the heap:   %p\n", heap_str);
+    printf("The address of the pointer in the stack: %p\n\n", &heap_str);
+
+    printf("The content of the global string:        %s\n", global_str);
+    printf("The address of the global string:        %p\n", global_str);
+    printf("The address of the global var in .data:  %p\n\n", &global_str);
+
+    printf("Is             (NULL == 0)? => The expression returned %d\n", (NULL == 0));
+    printf("Is ((uintptr_t) NULL == 0)? => The expression returned %d\n\n", ((uintptr_t) NULL == 0));
+
+    foo(1);
+    // scanf("%s", *heap_str); // For gdb
+    return 0;
+}
+
+int foo(int flag)
+{
+    int (*foo_ptr) (int) = foo;
+    if (flag) {
+        foo_ptr(0);
+    } else {
+    }
+
+    printf("This is in the function body of foo(), the value of passed argument: %d\n", flag);
+    return 0;
+}
